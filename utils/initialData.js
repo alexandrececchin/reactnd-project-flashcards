881c9e2c104fb743ed25c65cbb819eb73d21d936
Alexandre Rocha <alexandrececchin@gmail.com>
2019-03-13 15:27:48 -0300

    Add initial data

---
+++ utils/initialData.js
@@ -0,0 +1,77 @@
+const data = {
+  '415DscdVgxPhIf2pivUj': {
+    name: 'Lord of the rings',
+    id: '415DscdVgxPhIf2pivUj',
+    timestamp: 1552482720,
+    cards: [
+      {
+        id: '5jzuFhs05t7aLh3QCnua',
+        question: "Who's the author of Lord of the rings?",
+        answer: 'The author of the books is J.J.R.Tolken',
+        timestamp: 1552483139
+      },
+      {
+        id: '8PiViTLFaFpLyPHnJi4r',
+        question:
+          'Gandalf is the person who suggests a fellowship to destroy the one ring?',
+        answer:
+          "No. Frodo says he'd destroy it and Aragorn says he would help, then everybody else volunteers",
+        timestamp: 1552483134
+      },
+      {
+        id: '5Yh3CmNwgc7kXh5RRRXG',
+        question: 'Sauron is the lord of the Nazgul?',
+        answer: "Sauron doesn't have a physical form, he is the eye",
+        timestamp: 1552483129
+      },
+      {
+        id: '6oWyeoVpaCpQpYePgkWQ',
+        question:
+          'Legolas and Gimli have an ongoing competition of how many enemies they kill?',
+        answer:
+          'Yes. During the whole story they challenge to see who kills more orcs and uru kais, Legolas wins by far',
+        timestamp: 1552483126
+      },
+      {
+        id: '67eJMKASUmrjLQS1Rv44',
+        question: '3 members of the fellowship die by the end of the story?',
+        answer:
+          'Yes. Much debated one, but Boromir dies for good, while Frodo and Gandalf  "die" and earn a spot in the elvish afterlife',
+        timestamp: 1552483122
+      },
+      {
+        id: '3dLNxRlag4fk7Bj5qCLA',
+        question: 'Rohirin is the name given to the soldiers of Rohan?',
+        answer: 'No, it is the name gicen to their cavalary',
+        timestamp: 1552483118
+      }
+    ]
+  },
+  '81C9dD2rBeqZ8EDT4WVX': {
+    name: 'Game of Thrones',
+    id: '81C9dD2rBeqZ8EDT4WVX',
+    timestamp: 1552483269,
+    cards: [
+      {
+        id: 'wbcoxM9Y27oOrIylWUxl',
+        question: 'Who is jhon snow?',
+        answer: 'He is a bastard son of Ned Stark',
+        timestamp: 1552483139
+      },
+      {
+        id: '16jiaB9tSWStjYU512xT',
+        question: 'What is the Faceless Men?',
+        answer: 'A guild of assassins based in the Free City of Braavos',
+        timestamp: 1552483754
+      },
+      {
+        id: '1YCHC4mb87L7mWgCdERv',
+        question:
+          'Who discover that dragon glass could destroy the white walkers?',
+        answer:
+          "Sam, discover it while run from the beyond the wall back to the Night' Wacth base.",
+        timestamp: 1552483949
+      }
+    ]
+  }
+};
