6c900303fa8aa208a94255f267a4fe623275da9d
Alexandre Rocha <alexandrececchin@gmail.com>
2019-03-14 10:32:22 -0300

    Connect store to debugger

--- state/index.js
+++ state/index.js
@@ -1,14 +1,14 @@
-import { createStore, applyMiddleware } from "redux"
-import createSagaMiddleware from "redux-saga"
-import {all, takeLatest } from 'redux-saga/effects';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-import rootSaga from "./operationsSaga"
-import decks from "./reducers"
+import rootSaga from './operationsSaga';
+import decks from './reducers';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(decks, applyMiddleware(sagaMiddleware))
+const store = createStore(decks, composeWithDevTools(applyMiddleware(sagaMiddleware)))
 
-sagaMiddleware.run(rootSaga)
+sagaMiddleware.run(rootSaga);
 
-export default store
+export default store;
